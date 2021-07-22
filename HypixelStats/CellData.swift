bd8df5c963bc6f4d715f9bc928172ae0b13a3db1
codeplus <codeplus@CDSS-6116.local>
2021-07-22 18:01:29 -0400

    Added dynamic GameData struct and added dropdown wins to BB stats

---
+++ HypixelStats/CellData.swift
@@ -0,0 +1,15 @@
+//
+//  CellData.swift
+//  HypixelStats
+//
+//  Created by codeplus on 7/22/21.
+//
+
+import Foundation
+
+struct CellData {
+    var headerData: (String, Any)
+    var sectionData: [(String, Any)]
+    var isHeader: Bool
+    var isOpened: Bool
+}

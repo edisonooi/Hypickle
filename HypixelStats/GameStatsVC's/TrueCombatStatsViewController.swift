fcf267a69c55f7534f16f07c0a3ac2ec2ef94312
Edison Ooi <edison.ooi@duke.edu>
2021-08-15 23:36:04 -0400

    Added crazy walls stats

---
+++ HypixelStats/GameStatsVC's/TrueCombatStatsViewController.swift
@@ -0,0 +1,160 @@
+//
+//  TrueCombatStatsViewController.swift
+//  HypixelStats
+//
+//  Created by Edison Ooi on 8/15/21.
+//
+
+import Foundation
+import UIKit
+import SwiftyJSON
+
+class TrueCombatStatsViewController: GenericStatsViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = "Crazy Walls"
+        
+        statsTable.register(StatsInfoTableViewCell.nib(), forCellReuseIdentifier: StatsInfoTableViewCell.identifier)
+        statsTable.delegate = self
+        statsTable.dataSource = self
+        
+    }
+    
+    
+    lazy var statsTableData: [CellData] = {
+        
+        var ret: [CellData] = []
+        
+        var wins = data["wins"].intValue
+        var losses = data["losses"].intValue
+        var wlr = GameTypes.calculateRatio(numerator: wins, denominator: losses)
+        
+        var kills = data["kills"].intValue
+        var deaths = data["deaths"].intValue
+        var kdr = GameTypes.calculateRatio(numerator: kills, denominator: deaths)
+        
+        var arrowsShot = data["arrows_shot"].intValue
+        var arrowsHit = data["arrows_hit"].intValue
+        var arrowAccuracy = GameTypes.calculatePercentage(numerator: arrowsHit, denominator: arrowsShot)
+        
+        
+        var generalStats = [
+
+            CellData(headerData: ("Wins", wins), sectionData: [], isHeader: false, isOpened: false),
+            CellData(headerData: ("Losses", losses), sectionData: [], isHeader: false, isOpened: false),
+            CellData(headerData: ("W/L", wlr), sectionData: [], isHeader: false, isOpened: false),
+            
+            CellData(headerData: ("Kills", kills), sectionData: [], isHeader: false, isOpened: false),
+            CellData(headerData: ("Deaths", deaths), sectionData: [], isHeader: false, isOpened: false),
+            CellData(headerData: ("K/D", kdr), sectionData: [], isHeader: false, isOpened: false),
+            
+            CellData(headerData: ("Arrows Shot", arrowsShot), sectionData: [], isHeader: false, isOpened: false),
+            CellData(headerData: ("Arrows Hit", arrowsHit), sectionData: [], isHeader: false, isOpened: false),
+            CellData(headerData: ("Arrow Accuracy", arrowAccuracy), sectionData: [], isHeader: false, isOpened: false)
+        ]
+        
+        ret.append(contentsOf: generalStats)
+        
+        let modes = [
+            (id: "_solo",  name: "Solo Normal"),
+            (id: "_solo_chaos",  name: "Solo Lucky"),
+            (id: "_team",  name: "Teams Normal"),
+            (id: "_team_chaos",  name: "Teams Lucky"),
+        ]
+
+        var desiredStats = ["Wins", "Losses", "W/L", "Kills", "Deaths", "K/D"]
+
+        var modeStats: [CellData] = []
+
+        for mode in modes {
+            var statsForThisMode: [(String, Any)] = []
+
+            var modeWins = data["crazywalls_wins" + mode.id].intValue
+            var modeLosses = data["crazywalls_losses" + mode.id].intValue
+            var modeWLR = GameTypes.calculateRatio(numerator: modeWins, denominator: modeLosses)
+            
+            if modeWins + modeLosses == 0 {
+                continue
+            }
+
+            var modeKills = data["crazywalls_kills" + mode.id].intValue
+            var modeDeaths = data["crazywalls_deaths" + mode.id].intValue
+            var modeKDR = GameTypes.calculateRatio(numerator: modeKills, denominator: modeDeaths)
+
+            var dataForThisMode = [modeWins, modeLosses, modeWLR, modeKills, modeDeaths, modeKDR] as [Any]
+
+            for (index, category) in desiredStats.enumerated() {
+                statsForThisMode.append((category, dataForThisMode[index]))
+            }
+
+            modeStats.append(CellData(headerData: (mode.name, ""), sectionData: statsForThisMode, isHeader: false, isOpened: false))
+        }
+
+        ret.append(contentsOf: modeStats)
+        
+        
+        return ret
+    }()
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return statsTableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if statsTableData[section].isOpened {
+            return statsTableData[section].sectionData.count + 1
+        } else {
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = statsTable.dequeueReusableCell(withIdentifier: StatsInfoTableViewCell.identifier, for: indexPath) as! StatsInfoTableViewCell
+        
+        if indexPath.row == 0 {
+            let category = statsTableData[indexPath.section].headerData.0
+            let value = statsTableData[indexPath.section].headerData.1
+            cell.configure(category: category, value: "\(value)")
+        } else {
+            let category = statsTableData[indexPath.section].sectionData[indexPath.row - 1].0
+            let value = statsTableData[indexPath.section].sectionData[indexPath.row - 1].1
+            cell.configure(category: category, value: "\(value)")
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if !statsTableData[indexPath.section].sectionData.isEmpty && indexPath.row == 0 {
+            statsTableData[indexPath.section].isOpened = !statsTableData[indexPath.section].isOpened
+            let sections = IndexSet.init(integer: indexPath.section)
+            statsTable.reloadSections(sections, with: .none)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        if statsTableData[indexPath.section].sectionData.isEmpty || indexPath.row != 0 {
+            return false
+        }
+        
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let sectionsThatNeedHeader = [3, 6, 9]
+        
+        if sectionsThatNeedHeader.contains(section) {
+            return 32
+        }
+        
+        return CGFloat.leastNormalMagnitude
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat.leastNormalMagnitude
+    }
+}

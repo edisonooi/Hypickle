ac9ef9b4d31438fad056154c0d40db5dc0242477
Edison Ooi <edison.ooi@duke.edu>
2021-09-09 02:23:27 -0400

    Modified about screen

--- HypixelStats/AboutViewController.swift
+++ HypixelStats/AboutViewController.swift
@@ -13,18 +13,37 @@ class AboutViewController: UIViewController, UITextViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let mainText = "Powered by the Hypixel API.\n\nQuestions or suggestions? Please email "
+        let completeString = NSMutableAttributedString()
+        
+        let mainText = "Powered by the Hypixel API.\n\nQuestions or suggestions? Please email statshypickle@gmail.com\n\n\n\n\n\n"
+        
+        let attributionText = "App Icon is a modified version of pickle by Tyler Hanns from thenounproject.com"
+        let affiliationText = "\n\nHypickle is not affiliated with Hypixel."
         
         let attributes = [
             NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body),
             NSAttributedString.Key.foregroundColor: UIColor.label
         ]
         
-        let attributedString = NSMutableAttributedString(string: mainText, attributes: attributes)
+        let secondaryAttributes = [
+            NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .caption1),
+            NSAttributedString.Key.foregroundColor: UIColor.systemGray
+        ]
         
-        attributedString.setLink(url: "https://api.hypixel.net/", stringValue: "Hypixel API")
+        let mainString = NSMutableAttributedString(string: mainText, attributes: attributes)
+        let affiliationString = NSMutableAttributedString(string: affiliationText, attributes: secondaryAttributes)
+        let attributionString = NSMutableAttributedString(string: attributionText, attributes: secondaryAttributes)
         
-        textView.attributedText = attributedString
+        
+        mainString.setLink(url: "https://api.hypixel.net/", stringValue: "Hypixel API")
+        attributionString.setLink(url: "https://thenounproject.com/term/pickle/852963/", stringValue: "pickle")
+        
+        completeString.append(mainString)
+        completeString.append(attributionString)
+        completeString.append(affiliationString)
+        
+        
+        textView.attributedText = completeString
     }
     
     func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {

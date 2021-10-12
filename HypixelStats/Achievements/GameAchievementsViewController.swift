34f58eb370bc306c79529ffba958db78b1eae33c
Edison Ooi <edison.ooi@duke.edu>
2021-10-11 23:01:07 -0400

    Implemented sorting except for completion sorting

--- HypixelStats/Achievements/GameAchievementsViewController.swift
+++ HypixelStats/Achievements/GameAchievementsViewController.swift
@@ -8,6 +8,22 @@
 import UIKit
 import AMScrollingNavbar
 
+//TODO: Figure out how to sort by completed/incomplete
+
+enum OneTimeSortingCategory: String, CaseIterable {
+    case alphabetical = "Alphabetical (A to Z)"
+    case pointsAscending = "Points (Low to High)"
+    case pointsDescending = "Points (High to Low)"
+    case gamePercentAscending = "Game % Unlocked (Low to High)"
+    case gamePercentDescending = "Game % Unlocked (High to Low)"
+    case globalPercentAscending = "Global % Unlocked (Low to High)"
+    case globalPercentDescending = "Global % Unlocked (High to Low)"
+}
+
+enum TieredSortingCategory: String, CaseIterable {
+    case alphabetical = "Alphabetical (A to Z)"
+}
+
 class GameAchievementsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var achievementGameID: String = ""
@@ -16,6 +32,12 @@ class GameAchievementsViewController: UIViewController, UITableViewDataSource, U
     
     var oneTimeAchievementsSorted: [(String, OneTimeAchievement)] = []
     var tieredAchievementsSorted: [(String, TieredAchievement)] = []
+    
+    var selectedOneTimeSortingRow = 0
+    var selectedTieredSortingRow = 0
+    
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
 
     @IBOutlet weak var achievementsTable: UITableView!
     
@@ -132,7 +154,192 @@ class GameAchievementsViewController: UIViewController, UITableViewDataSource, U
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 || section == 1 {
+            return 100
+        }
+        
+        return CGFloat.leastNormalMagnitude
+    }
     
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        switch section {
+        case 0:
+            let headerView = SortTableHeaderView.instanceFromNib()
+            headerView.headerLabel.text = "Tiered Achievements"
+            headerView.sortButton.addTarget(self, action: #selector(sortTieredButtonTapped), for: .touchUpInside)
+            headerView.sortButton.titleLabel?.text = "Sorted: " + getShortSortingCategoryName(category: TieredSortingCategory.allCases[selectedTieredSortingRow])
+            return headerView
+        case 1:
+            let headerView = SortTableHeaderView.instanceFromNib()
+            headerView.headerLabel.text = "One-Time Achievements"
+            headerView.sortButton.addTarget(self, action: #selector(sortOneTimeButtonTapped), for: .touchUpInside)
+            headerView.sortButton.titleLabel?.text = "Sorted: " + getShortSortingCategoryName(category: OneTimeSortingCategory.allCases[selectedOneTimeSortingRow])
+            return headerView
+        default:
+            return nil
+        }
+    }
+    
+    @objc func sortTieredButtonTapped() {
+        let vc = UIViewController()
+        vc.preferredContentSize = CGSize(width: screenWidth, height: screenHeight / 5)
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight / 5))
+        pickerView.tag = 0
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        pickerView.selectRow(selectedTieredSortingRow, inComponent: 0, animated: false)
+        
+        vc.view.addSubview(pickerView)
+        pickerView.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor).isActive = true
+        pickerView.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor).isActive = true
+        
+        let alert = UIAlertController(title: "Sort By...", message: "", preferredStyle: .actionSheet)
+        
+        alert.setValue(vc, forKey: "contentViewController")
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (UIAlertAction) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Select", style: .default, handler: { (UIAlertAction) in
+            self.selectedTieredSortingRow = pickerView.selectedRow(inComponent: 0)
+            
+            let selectedCategory = TieredSortingCategory.allCases[self.selectedTieredSortingRow]
+            
+            self.sortTiered(category: selectedCategory)
+            
+//            let section = IndexSet.init(integer: 0)
+//            self.achievementsTable.reloadSections(section, with: .none)
+            self.achievementsTable.reloadData()
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func sortOneTimeButtonTapped() {
+        let vc = UIViewController()
+        vc.preferredContentSize = CGSize(width: screenWidth, height: screenHeight / 5)
+        let pickerView = UIPickerView(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight / 5))
+        pickerView.tag = 1
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        pickerView.selectRow(selectedOneTimeSortingRow, inComponent: 0, animated: false)
+        
+        vc.view.addSubview(pickerView)
+        pickerView.centerXAnchor.constraint(equalTo: vc.view.centerXAnchor).isActive = true
+        pickerView.centerYAnchor.constraint(equalTo: vc.view.centerYAnchor).isActive = true
+        
+        let alert = UIAlertController(title: "Sort By...", message: "", preferredStyle: .actionSheet)
+        
+        alert.setValue(vc, forKey: "contentViewController")
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (UIAlertAction) in
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Select", style: .default, handler: { (UIAlertAction) in
+            self.selectedOneTimeSortingRow = pickerView.selectedRow(inComponent: 0)
+            
+            let selectedCategory = OneTimeSortingCategory.allCases[self.selectedOneTimeSortingRow]
+            
+            self.sortOneTimes(category: selectedCategory)
+            
+//            let section = IndexSet.init(integer: 1)
+//            self.achievementsTable.reloadSections(section, with: .none)
+            self.achievementsTable.reloadData()
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func getShortSortingCategoryName(category: OneTimeSortingCategory) -> String {
+        switch category {
+        case .alphabetical:
+            return "A to Z"
+        case .pointsAscending:
+            return "Points ↑"
+        case .pointsDescending:
+            return "Points ↓"
+        case .gamePercentAscending:
+            return "Game % ↑"
+        case .gamePercentDescending:
+            return "Game % ↓"
+        case .globalPercentAscending:
+            return "Global % ↑"
+        case .globalPercentDescending:
+            return "Global % ↓"
+        }
+    }
+    
+    func getShortSortingCategoryName(category: TieredSortingCategory) -> String {
+        switch category {
+        case .alphabetical:
+            return "A to Z"
+        }
+    }
+    
+    func sortOneTimes(category: OneTimeSortingCategory) {
+        switch category {
+        case .alphabetical:
+            oneTimeAchievementsSorted.sort {
+                $0.1.name < $1.1.name
+            }
+        case .pointsAscending:
+            oneTimeAchievementsSorted.sort {
+                $0.1.points < $1.1.points
+            }
+        case .pointsDescending:
+            oneTimeAchievementsSorted.sort {
+                $0.1.points > $1.1.points
+            }
+        case .gamePercentAscending:
+            oneTimeAchievementsSorted.sort {
+                $0.1.gamePercentUnlocked < $1.1.gamePercentUnlocked
+            }
+        case .gamePercentDescending:
+            oneTimeAchievementsSorted.sort {
+                $0.1.gamePercentUnlocked > $1.1.gamePercentUnlocked
+            }
+        case .globalPercentAscending:
+            oneTimeAchievementsSorted.sort {
+                $0.1.globalPercentUnlocked < $1.1.globalPercentUnlocked
+            }
+        case .globalPercentDescending:
+            oneTimeAchievementsSorted.sort {
+                $0.1.globalPercentUnlocked > $1.1.globalPercentUnlocked
+            }
+        }
+        
+    }
+    
+    func sortTiered(category: TieredSortingCategory) {
+        switch category {
+        case .alphabetical:
+            tieredAchievementsSorted.sort {
+                $0.1.name < $1.1.name
+            }
+        }
+    }
+}
 
-
+extension GameAchievementsViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 0 {
+            return TieredSortingCategory.allCases.count
+        }
+        
+        return OneTimeSortingCategory.allCases.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 0 {
+            return TieredSortingCategory.allCases[row].rawValue
+        }
+        
+        return OneTimeSortingCategory.allCases[row].rawValue
+    }
 }
---
+++ HypixelStats/Custom Views/TableView Headers/SortTableHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SortTableHeaderView.swift
+//  HypixelStats
+//
+//  Created by Edison Ooi on 10/11/21.
+//
+
+import UIKit
+
+class SortTableHeaderView: UIView {
+
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var sortButton: UIButton!
+    
+    
+    class func instanceFromNib() -> SortTableHeaderView {
+        return UINib(nibName: "SortTableHeaderView", bundle: Bundle(for: SortTableHeaderView.self)).instantiate(withOwner: nil, options: nil)[0] as! SortTableHeaderView
+    }
+
+}

239072ec5b1c2ac847bd0fce4b90c5c1c1ba34bf
Edison Ooi <edison.ooi@duke.edu>
2021-10-15 13:21:11 -0400

    Added colors reference

---
+++ HypixelStats/Utils/Colors.swift
@@ -0,0 +1,35 @@
+//
+//  Colors.swift
+//  HypixelStats
+//
+//  Created by Edison Ooi on 10/15/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    struct MinecraftColors {
+        static var aqua = UIColor(named: "mc_aqua")!
+        static var darkBlue = UIColor(named: "mc_dark_blue")!
+        static var darkGreen = UIColor(named: "mc_dark_green")!
+        static var darkAqua = UIColor(named: "mc_dark_aqua")!
+        static var darkRed = UIColor(named: "mc_dark_red")!
+        static var darkPurple = UIColor(named: "mc_dark_purple")!
+        static var gold = UIColor(named: "mc_gold")!
+        static var gray = UIColor(named: "mc_gray")!
+        static var darkGray = UIColor(named: "mc_dark_gray")!
+        static var blue = UIColor(named: "mc_blue")!
+        static var green = UIColor(named: "mc_green")!
+        static var red = UIColor(named: "mc_red")!
+        static var lightPurple = UIColor(named: "mc_light_purple")!
+        static var yellow = UIColor(named: "mc_yellow")!
+        static var bronze = UIColor(named: "mc_bronze")!
+        static var cyan = UIColor(named: "mc_cyan")!
+        static var pink = UIColor(named: "mc_pink")!
+        static var white = UIColor(named: "mc_white")!
+        static var black = UIColor.black
+    }
+    
+    static var gold = UIColor(named: "white_label")!
+}
